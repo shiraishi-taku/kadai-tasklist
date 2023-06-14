3d112114d181d5dfc31304d00fe86155f3b19a39
taku.shiraishi <tshiraishi@unisrv.jp>
2023-06-14 10:45:43 +0900

    コミット

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,11 +7,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+
 @Entity
-@Table(name = "messages")
-public class tasks {
+@NamedQueries({
+    @NamedQuery(
+         name = "getAllTasks",
+         query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(t) FROM Task AS t"
+            )
+})
+@Table(name = "tasks")
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
